eba36f31c1e2f8667ae45b1daf82ed202cb22354
Gowthaman <gowthaman.nkl1@gmail.com>
2023-03-27 10:39:47 +0530

    remove all

--- privacy-policy.php
+++
@@ -1,9 +0,0 @@
-     <?php include ('header.php')?>
-
-     
-      <!-- End Page Header -->        
-      <div class="container" style=" height:500px;">
-          <h2 style="text-align:center; padding-top:200px;"><b>COMING SOON</b></h2>
-      </div>
-    
-         <?php include ('footer.php')?>
